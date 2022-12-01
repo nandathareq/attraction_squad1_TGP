b056f3f48e92475caf2801a9e979f40b2ebf89cd
Nanda Thareq <nanda.thareq@gmail.com>
2022-12-01 15:52:25 +0700

    Resi Model
    update

---
+++ attractions/lib/model/resi_model.dart
@@ -0,0 +1,17 @@
+class ResiModel {
+  final String total;
+  final String date;
+  final String idTransaksi;
+  final String nasabah;
+  final String rekening;
+  final String kodeBooking;
+
+  ResiModel({
+    required this.total,
+    required this.date,
+    required this.idTransaksi,
+    required this.nasabah,
+    required this.rekening,
+    required this.kodeBooking,
+  });
+}
