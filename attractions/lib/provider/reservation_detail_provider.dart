3b3bcf42cb569163795947bfa957f4e147c41eb0
Nanda Thareq <nanda.thareq@gmail.com>
2022-12-01 15:49:16 +0700

    screen resi unstyled
    belum styling tapi udah bisa jalan

--- attractions/lib/provider/reservation_detail_provider.dart
+++ attractions/lib/provider/reservation_detail_provider.dart
@@ -4,12 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../model/reservation_model.dart';
+import '../model/resi_model.dart';
 
 class ReservationProvider with ChangeNotifier {
   List<ReservationModel> _resers = <ReservationModel>[];
 
   List<ReservationModel> get resers {
     return [..._resers];
+  }
+
+  ResiModel? _dataResi;
+
+  ResiModel? get dataResi {
+    return _dataResi;
   }
 
   Future<void> fetchCodeBoking(kode) async {
@@ -40,6 +47,27 @@ class ReservationProvider with ChangeNotifier {
       // });
 
       _resers = loadedDatas;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> fetchResi() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'http://10.10.62.220:5000/api/v1/subInvoice/detail?bookingCode=gb14lo'));
+
+      final resiData = json.decode(response.body);
+
+      final data = ResiModel(
+          total: resiData['total'].toString(),
+          date: resiData['paidDate'].toString(),
+          idTransaksi: resiData['idTransaksi'],
+          nasabah: resiData['user']['userName'],
+          rekening: "akjdhflkasdj",
+          kodeBooking: resiData['bookingCode']);
+
+      _dataResi = data;
     } catch (e) {
       rethrow;
     }
