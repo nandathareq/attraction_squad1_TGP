1ad0a1919c920be5ec92973ab12fe11acc9c0166
Fachry <fachryfirdaus277@yahoo.com>
2022-12-01 14:33:12 +0700

    pin payment

--- attractions/macos/Flutter/GeneratedPluginRegistrant.swift
+++ attractions/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import path_provider_macos
 import share_plus
+import smart_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
 }
